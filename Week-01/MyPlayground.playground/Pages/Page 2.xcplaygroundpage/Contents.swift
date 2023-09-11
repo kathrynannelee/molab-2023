92ebdf037cd2e52fe62ce4268f2f2ee63b20863e
kathrynannelee <kathryntamlee@gmail.com>
2023-09-10 23:33:11 -0400

    100 days of swiftui
    testing variables and consonants

--- Week-01/MyPlayground.playground/Pages/Page 2.xcplaygroundpage/Contents.swift
+++ Week-01/MyPlayground.playground/Pages/Page 2.xcplaygroundpage/Contents.swift
@@ -6,4 +6,12 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 
+var name = "Katy"
+print(name)
+name = "Eloise"
+print(name)
+name = "Francesca"
+print(name)
+
+let character = "Daphne"
 
