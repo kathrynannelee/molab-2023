ea0c6be21cc309e49616f167b9c9aec1a03019d2
kathrynannelee <kathryntamlee@gmail.com>
2023-09-13 22:30:14 -0400

    100 days of swiftui
    day 3

--- Week-01/MyPlayground.playground/Pages/Day3.xcplaygroundpage/Contents.swift
+++ Week-01/MyPlayground.playground/Pages/Day3.xcplaygroundpage/Contents.swift
@@ -5,3 +5,102 @@ import Foundation
 var greeting = "Hello, playground"
 
 //: [Next](@next)
+//arrays
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+//append to add to an array, must be of like type
+beatles.append("Allen")
+beatles.append("Adrian")
+beatles.append("Novall")
+beatles.append("Vivian")
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+var albums = [String]()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+//equivalent method
+var albums1 = ["Folklore"]
+albums1.append("Fearless")
+albums1.append("Red")
+
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+//remove
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+//check if contains
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+//sort array
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+//reverse array
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
+
+//dictionaries of data
+//not as accurate
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+//more detailed
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+//print(employee2["name"])
+//print(employee2["job"])
+//print(employee2["location"])
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+//set
+let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people)
+var people1 = Set<String>()
+people1.insert("Denzel Washington")
+people1.insert("Tom Cruise")
+
+//enumerations
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+var day = Weekday.monday
+day = .tuesday
+day = .friday
