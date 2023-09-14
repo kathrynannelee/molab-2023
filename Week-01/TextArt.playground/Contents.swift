cc09ecfa437dc61451061bc02a8b9d9e263721ba
kathrynannelee <kathryntamlee@gmail.com>
2023-09-13 23:09:22 -0400

    text art bunnies

--- Week-01/TextArt.playground/Contents.swift
+++ Week-01/TextArt.playground/Contents.swift
@@ -2,3 +2,34 @@ import UIKit
 
 var greeting = "Hello, playground"
 
+var ear = "(\\"
+var face = "( -.-)"
+var tail = "o_"
+var foot = "(\")"
+
+func printSpace() -> String{
+    return "     "
+}
+
+func printSmallSpace() -> String{
+    return "   "
+}
+
+func printEars() -> String{
+    return "" + ear + "  " + ear
+}
+
+func printFace() -> String{
+    return "" + face
+}
+
+func printBottom() -> String{
+    return tail + foot + foot
+}
+
+for _ in 1...3{
+    print(printEars() + printSpace() + printEars() + printSpace() + printEars())
+    print(printFace() + printSpace() + printFace() + printSpace() + printFace())
+    print(printBottom() + printSmallSpace() + printBottom() + printSmallSpace() + printBottom())
+    print("")
+}
