a3ef442b96815cc8ae16b586cf73a321c8af7ab9
kathrynannelee <kathryntamlee@gmail.com>
2023-09-21 00:49:10 -0400

    checkpoint 5

---
+++ Week-02/100DaysOfSwiftUI.playground/Pages/Checkpoint 5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+//With closures under your belt, it’s time to try a little coding challenge using them.
+//
+//You’ve already met sorted(), filter(), map(), so I’d like you to put them together in a chain – call one, then the other, then the other back to back without using temporary variables.
+//
+//Your input is this:
+//
+//let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+//Your job is to:
+//
+//Filter out any numbers that are even
+//Sort the array in ascending order
+//Map them to strings in the format “7 is a lucky number”
+//Print the resulting array, one item per line
+//So, your output should be as follows:
+//
+//7 is a lucky number
+//15 is a lucky number
+//21 is a lucky number
+//31 is a lucky number
+//33 is a lucky number
+//49 is a lucky number
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+var luckyNumbersSorted = luckyNumbers.sorted()
+
+print(luckyNumbersSorted)
+
+let numbersFiltered = luckyNumbersSorted.filter { $0 % 2 != 0 }
+print(numbersFiltered)
+
+//let isALuckyNumber = numbersFiltered.map { " \$0  is a lucky number" }
+//
+//print(isALuckyNumber)
