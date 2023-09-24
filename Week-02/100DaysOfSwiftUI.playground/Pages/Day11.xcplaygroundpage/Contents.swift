3d4c27f9171995744375c5e163a91e4a86ff3b71
kathrynannelee <kathryntamlee@gmail.com>
2023-09-23 22:32:05 -0400

    100 days of swiftui
    day 11 + checkpoint 6

--- Week-02/100DaysOfSwiftUI.playground/Pages/Day11.xcplaygroundpage/Contents.swift
+++ Week-02/100DaysOfSwiftUI.playground/Pages/Day11.xcplaygroundpage/Contents.swift
@@ -32,3 +32,16 @@ if success {
 } else {
     print("Failed to get the money")
 }
+
+//static functionality -- doesn’t exist uniquely on instances of the struct.
+//also explains why we’re able to modify the studentCount property without marking the method as mutating – that’s only needed with regular struct functions for times when an instance of struct was created as a constant, and there is no instance when calling add()
+struct School {
+    static var studentCount = 0
+
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+School.add(student: "Taylor Swift")
+print(School.studentCount)
--- Week-03/Week03/Week03/ContentView.swift
+++ Week-03/Week03/Week03/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-                .font(.largeTitle)
+                //.font(.largeTitle)
         }
         .padding()
     }
