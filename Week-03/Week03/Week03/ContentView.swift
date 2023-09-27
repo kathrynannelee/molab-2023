442ea0b314af6e981e5a535afa8196bdf6a84dcb
kathrynannelee <kathryntamlee@gmail.com>
2023-09-27 00:04:42 -0400

    singleview swiftui app -- week03 homework

--- Week-03/Week03/Week03/ContentView.swift
+++ Week-03/Week03/Week03/ContentView.swift
@@ -7,14 +7,47 @@
 
 import SwiftUI
 
+var sfSymbols = ["paperplane", "bolt", "globe", "trash", "barcode", "book", "magnifyingglass", "person.3", "hand.thumbsup", "heart.circle", "star"]
+
+var letters = ["A", "E", "S", "O", "R", "I", "L", "T", "N", "U"]
+
+func pickRandomInt() -> Int{
+    var randomInt = Int.random(in: 0...10)
+    return randomInt
+}
+
+//var autumnEmojis = ["🎃", "🧣", "🍎", "🍂"]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            
+            Text(letters[pickRandomInt()])
+                .font(.largeTitle)
+            Image(systemName: sfSymbols[pickRandomInt()])
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                //.font(.largeTitle)
+            
+            Text(letters[pickRandomInt()])                .font(.largeTitle)
+            Image(systemName: sfSymbols[pickRandomInt()])
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text(letters[pickRandomInt()])                .font(.largeTitle)
+            Image(systemName: sfSymbols[pickRandomInt()])
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text(letters[pickRandomInt()])                .font(.largeTitle)
+            Image(systemName: sfSymbols[pickRandomInt()])
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text(letters[pickRandomInt()])                .font(.largeTitle)
+            Image(systemName: sfSymbols[pickRandomInt()])
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+
         }
         .padding()
     }
