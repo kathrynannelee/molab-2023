80079f8190cc304d3f090b05f922ee31a03e4cd3
kathrynannelee <kathryntamlee@gmail.com>
2023-10-03 23:00:17 -0400

    Week 4 Updates

---
+++ Week-04/Escape Room/Escape Room/Escape_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Escape_RoomApp.swift
+//  Escape Room
+//
+//  Created by Kathryn Lee on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Escape_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
