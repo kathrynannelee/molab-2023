18deb50ed3ddab8ec670ec1e4f7d6737808cae6a
kathrynannelee <kathryntamlee@gmail.com>
2023-10-04 19:09:52 -0400

    building lists and navigation swiftui

--- Week-04/Building Lists and Navigation/StartingPoint/Landmarks/ContentView.swift
+++ Week-04/Building Lists and Navigation/StartingPoint/Landmarks/ContentView.swift
@@ -1,22 +1,26 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Kathryn Lee on 10/3/23.
-//
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            MapView().frame(height: 300)
-            
-            CircleImage().offset(y: -130)
+            MapView()
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
                 .padding(.bottom, -130)
-            VStack(alignment: .leading){
+
+            VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+
                 HStack {
                     Text("Joshua Tree National Park")
                     Spacer()
@@ -24,19 +28,20 @@ struct ContentView: View {
                 }
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
+
                 Divider()
+
                 Text("About Turtle Rock")
                     .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
-            
+
             Spacer()
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
---
+++ Week-04/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Kathryn Lee on 10/4/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(Circle())
+        .overlay{
+            Circle().stroke(.white, lineWidth: 4)
+        }
+        .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
