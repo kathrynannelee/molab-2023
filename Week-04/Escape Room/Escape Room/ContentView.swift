db8a56e8585b39be1c513abfc1edd02eaffa4172
kathrynannelee <kathryntamlee@gmail.com>
2023-10-03 23:31:36 -0400

    SwiftUI Tutorials + Timer

--- Week-04/Escape Room/Escape Room/ContentView.swift
+++ Week-04/Escape Room/Escape Room/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentDate = Date.now
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack {
+            Text("\(currentDate)")
+                .onReceive(timer) { input in
+                    currentDate = input
+                }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
