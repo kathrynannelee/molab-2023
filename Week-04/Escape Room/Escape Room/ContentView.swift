7983351c42d10cdd360fcc6709754a009ebf6664
kathrynannelee <kathryntamlee@gmail.com>
2023-10-05 21:46:19 -0400

    Update ContentView.swift

--- Week-04/Escape Room/Escape Room/ContentView.swift
+++ Week-04/Escape Room/Escape Room/ContentView.swift
@@ -7,36 +7,49 @@
 
 import SwiftUI
 
+struct ResultView: View {
+    var choice: String
+
+    var body: some View {
+        Text("You chose \(choice)")
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingDetailView = false
     @State var timeRemaining = 60
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         NavigationView {
-            NavigationLink(destination: Text("Second View"), isActive: $isShowingDetailView) {
-                        Text("Show Detail")
-                    }
-                    .navigationTitle("Navigation")
+            VStack(spacing: 30) {
+                Text("You're going to flip a coin – do you want to choose heads or tails?")
+                
+                NavigationLink(destination: ResultView(choice: "Heads")) {
+                    Text("Choose Heads")
                 }
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        self.isShowingDetailView = false
-                    }
+                
+                NavigationLink(destination: ResultView(choice: "Tails")) {
+                    Text("Choose Tails")
                 }
-                //Text("\(timeRemaining)")
-                //.font(.title)
-                //.fontWeight(.heavy)
-                //.foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-                //.onReceive(timer) { _ in
-                            //if timeRemaining > 0 {
-                              //  timeRemaining -= 1
-                            //}
-                       // }
+            }
+            .navigationTitle("Navigation")
+            
+            //Text("\(timeRemaining)")
+            //.font(.title)
+            //.fontWeight(.heavy)
+            //.foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            //.onReceive(timer) { _ in
+            //if timeRemaining > 0 {
+            //  timeRemaining -= 1
+            //}
+            // }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
