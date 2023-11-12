0b96a03115ff5a05b3659a953fac2c6ff3164873
kathrynannelee <kathryntamlee@gmail.com>
2023-11-12 17:46:30 -0500

    hacking with swift: bookworm app

---
+++ Week-10/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kathryn Lee on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
